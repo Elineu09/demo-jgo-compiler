5ff8e1314a1541bca4c0e80d638e0c429862c46c
Elineu <elineutomas4@gmail.com>
2026-05-16 19:40:26 +0100

    checkout Program.java

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,24 @@
 package application;
 
 import java.util.List;
+
 import model.entities.Lexer;
+import model.entities.Parser;
 import util.FileReaderUtil;
 
 public class Program {
     public static void main(String[] args) {
         String path = "C:/temp/compiladores/programa.go"; 
         
+        System.out.println("### INICIANDO ANÁLISE LÉXICA ###");
         List<String> sourceCode = FileReaderUtil.readSourceFile(path);
         
         Lexer lexer = new Lexer(sourceCode);
         lexer.analex(); 
+        
+        System.out.println("### INICIANDO ANÁLISE SINTÁTICA ###");
+        Parser parser = new Parser(lexer.getTokens());
+        
+        parser.parse();
     }
 } 
